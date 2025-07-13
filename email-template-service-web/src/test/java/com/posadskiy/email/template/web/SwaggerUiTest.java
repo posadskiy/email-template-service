172aee41a6592933c1d43a31c4896056a564a9c3
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2025-07-13 22:21:28 +0200

    Fix tests

---
+++ email-template-service-web/src/test/java/com/posadskiy/email/template/web/SwaggerUiTest.java
@@ -0,0 +1,33 @@
+package com.posadskiy.email.template.web;
+
+import io.micronaut.core.io.ResourceLoader;
+import io.micronaut.http.client.BlockingHttpClient;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@MicronautTest
+class SwaggerUiTest {
+
+    @Test
+    void shouldServeOpenApiSpecification(@Client("/") HttpClient httpClient) {
+        BlockingHttpClient client = httpClient.toBlocking();
+        assertDoesNotThrow(() -> client.exchange("/swagger/swagger.yml"));
+    }
+
+    @Test
+    void shouldGenerateSwaggerUiResources(ResourceLoader resourceLoader) {
+        assertTrue(resourceLoader.getResource("META-INF/swagger/views/swagger-ui/index.html").isPresent());
+    }
+
+    @Test
+    void shouldServeSwaggerUi(@Client("/") HttpClient httpClient) {
+        BlockingHttpClient client = httpClient.toBlocking();
+        assertDoesNotThrow(() -> client.exchange("/swagger-ui/index.html"));
+    }
+
+} 
